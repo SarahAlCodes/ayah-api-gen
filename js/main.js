d53c38928690edd5b0bf37c43afef8d930e555f4
SarahAlCodes <salsaid1016@gmail.com>
2022-06-07 00:51:03 -0400

    add new verses

--- js/main.js
+++ js/main.js
@@ -1,50 +1,26 @@
-// document.querySelector('button').addEventListener('click', apiRequest)
-
-// async function apiRequest(){
-//     const teaName = document.querySelector('input').value
-//     try{
-//         const response = await fetch(`https://ayah-api-gen.herokuapp.com/api/${teaName}`)
-//         const data = await response.json()
-//         console.log(data)
-
-//         document.getElementById('').innerText = data.
-//         document.getElementById('').innerText = data.
-//         document.getElementById('').innerText = data.
-//         document.getElementById('').innerText = data.
-            
-//     }catch(error){
-//         console.log(error)
-//     }
-// }
-
-
-// //Example fetch using pokemonapi.co
 document.querySelector('button').addEventListener('click', apiRequest)
 
 function apiRequest(){
+  var ayah = Math.floor(Math.random() * 6236) + 1 
   const surahName = document.querySelector('input').value
   console.log(surahName)
 
   const url = `https://ayah-api-gen.herokuapp.com/api/${surahName}`
-
+    
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
 
         console.log(data)
-        document.getElementById('#arabicText').innerText = data.arabicVerseText
-        document.getElementById('#englishText').innerText = data.verseText
-        document.querySelector('#surahInfo').innerText = data.surahAndAyah
+        document.getElementById('arabicText').innerText = data.arabicVerseText
+        document.getElementById('englishText').innerText = data.verseText
+        document.getElementById('surahInfo').innerText = data.surahAndAyah
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
 }
-
-
-
-
 
 
 
--- server.js
+++ server.js
@@ -21,6 +21,46 @@ const surah = {
         'verseText': 'The revelation of this Book is from Allah—the Almighty, All-Knowing',
         'surahAndAyah': '40:2'
     },
+    'baqarah':{
+        'arabicVerseText': 'ذَٰلِكَ ٱلْكِتَـٰبُ لَا رَيْبَ ۛ فِيهِ ۛ هُدًۭى لِّلْمُتَّقِينَ',
+        'verseText': 'This is the Book! There is no doubt about it—a guide for those mindful of Allah',
+        'surahAndAyah': '2:2'
+    },
+    'imran':{
+        'arabicVerseText': 'إِنَّ ٱلدِّينَ عِندَ ٱللَّهِ ٱلْإِسْلَـٰمُ ۗ وَمَا ٱخْتَلَفَ ٱلَّذِينَ أُوتُوا۟ ٱلْكِتَـٰبَ إِلَّا مِنۢ بَعْدِ مَا جَآءَهُمُ ٱلْعِلْمُ بَغْيًۢا بَيْنَهُمْ ۗ وَمَن يَكْفُرْ بِـَٔايَـٰتِ ٱللَّهِ فَإِنَّ ٱللَّهَ سَرِيعُ ٱلْحِسَابِ',
+        'verseText': 'Certainly, Allah’s only Way is Islam.1 Those who were given the Scripture did not dispute ˹among themselves˺ out of mutual envy until knowledge came to them. Whoever denies Allah’s signs, then surely Allah is swift in reckoning.',
+        'surahAndAyah': '3:19'
+    },
+    'an-nisa':{
+        'arabicVerseText': 'يَـٰٓأَيُّهَا ٱلَّذِينَ ءَامَنُوا۟ لَا تَأْكُلُوٓا۟ أَمْوَٰلَكُم بَيْنَكُم بِٱلْبَـٰطِلِ إِلَّآ أَن تَكُونَ تِجَـٰرَةً عَن تَرَاضٍۢ مِّنكُمْ ۚ وَلَا تَقْتُلُوٓا۟ أَنفُسَكُمْ ۚ إِنَّ ٱللَّهَ كَانَ بِكُمْ رَحِيمًۭا',
+        'verseText': 'O believers! Do not devour one another’s wealth illegally, but rather trade by mutual consent. And do not kill ˹each other or˺ yourselves. Surely Allah is ever Merciful to you.',
+        'surahAndAyah': '4:29'
+    },
+    'al-maidah':{
+        'arabicVerseText': 'قَالَ رَبِّ إِنِّى لَآ أَمْلِكُ إِلَّا نَفْسِى وَأَخِى ۖ فَٱفْرُقْ بَيْنَنَا وَبَيْنَ ٱلْقَوْمِ ٱلْفَـٰسِقِينَ',
+        'verseText': 'Moses pleaded, “My Lord! I have no control over anyone except myself and my brother. So set us apart from the rebellious people.”',
+        'surahAndAyah': '5:25'
+    },
+    'al-anam':{
+        'arabicVerseText': 'فَلَمَّا جَنَّ عَلَيْهِ ٱلَّيْلُ رَءَا كَوْكَبًۭا ۖ قَالَ هَـٰذَا رَبِّى ۖ فَلَمَّآ أَفَلَ قَالَ لَآ أُحِبُّ ٱلْـَٔافِلِينَ',
+        'verseText': 'When the night grew dark upon him, he saw a star and said, “This is my Lord!” But when it set, he said, “I do not love things that set.”',
+        'surahAndAyah': '6:76'
+    },
+    'al-araf':{
+        'arabicVerseText': 'وَلَا تُفْسِدُوا۟ فِى ٱلْأَرْضِ بَعْدَ إِصْلَـٰحِهَا وَٱدْعُوهُ خَوْفًۭا وَطَمَعًا ۚ إِنَّ رَحْمَتَ ٱللَّهِ قَرِيبٌۭ مِّنَ ٱلْمُحْسِنِينَ',
+        'verseText': 'Do not spread corruption in the land after it has been set in order. And call upon Him with hope and fear. Indeed, Allah’s mercy is always close to the good-doers.',
+        'surahAndAyah': '7:56'
+    },
+    'al-anfal':{
+        'arabicVerseText': 'إِنَّ شَرَّ ٱلدَّوَآبِّ عِندَ ٱللَّهِ ٱلَّذِينَ كَفَرُوا۟ فَهُمْ لَا يُؤْمِنُونَ',
+        'verseText': 'Indeed, the worst of all beings in the sight of Allah are those who persist in disbelief, never to have faith—',
+        'surahAndAyah': '8:55'
+    },
+    'at-tawbah':{
+        'arabicVerseText': 'يُرِيدُونَ أَن يُطْفِـُٔوا۟ نُورَ ٱللَّهِ بِأَفْوَٰهِهِمْ وَيَأْبَى ٱللَّهُ إِلَّآ أَن يُتِمَّ نُورَهُۥ وَلَوْ كَرِهَ ٱلْكَـٰفِرُونَ',
+        'verseText': 'They wish to extinguish Allah’s light1 with their mouths, but Allah will only allow His light to be perfected, even to the dismay of the disbelievers.',
+        'surahAndAyah': '9:32'
+    },             
     'unknown':{
         'verseText': 'unknown',
         'surahAndAyah': 'unknown'
