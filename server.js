8f7717c2b9077bdba428249c11d767b8172651ca
SarahAlCodes <salsaid1016@gmail.com>
2022-06-07 11:15:10 -0400

    added more surahs

--- server.js
+++ server.js
@@ -6,21 +6,6 @@ const PORT = 8000
 app.use(cors())
 
 const surah = {
-    'maryam':{
-        'arabicVerseText': 'فَنَادَىٰهَا مِن تَحْتِهَآ أَلَّا تَحْزَنِى قَدْ جَعَلَ رَبُّكِ تَحْتَكِ سَرِيًّۭا',
-        'verseText': 'So a voice reassured her from below her, “Do not grieve! Your Lord has provided a stream at your feet.',
-        'surahAndAyah': '19:24'
-    },
-    'ibrahim':{
-        'arabicVerseText': 'وَمَثَلُ كَلِمَةٍ خَبِيثَةٍۢ كَشَجَرَةٍ خَبِيثَةٍ ٱجْتُثَّتْ مِن فَوْقِ ٱلْأَرْضِ مَا لَهَا مِن قَرَارٍۢ ',
-        'verseText': 'And the parable of an evil word is that of an evil tree, uprooted from the earth, having no stability..',
-        'surahAndAyah': '14:26'
-    },
-    'ghafir':{
-        'arabicVerseText': 'تَنزِيلُ ٱلْكِتَـٰبِ مِنَ ٱللَّهِ ٱلْعَزِيزِ ٱلْعَلِيمِ',
-        'verseText': 'The revelation of this Book is from Allah—the Almighty, All-Knowing',
-        'surahAndAyah': '40:2'
-    },
     'baqarah':{
         'arabicVerseText': 'ذَٰلِكَ ٱلْكِتَـٰبُ لَا رَيْبَ ۛ فِيهِ ۛ هُدًۭى لِّلْمُتَّقِينَ',
         'verseText': 'This is the Book! There is no doubt about it—a guide for those mindful of Allah',
@@ -28,12 +13,12 @@ const surah = {
     },
     'imran':{
         'arabicVerseText': 'إِنَّ ٱلدِّينَ عِندَ ٱللَّهِ ٱلْإِسْلَـٰمُ ۗ وَمَا ٱخْتَلَفَ ٱلَّذِينَ أُوتُوا۟ ٱلْكِتَـٰبَ إِلَّا مِنۢ بَعْدِ مَا جَآءَهُمُ ٱلْعِلْمُ بَغْيًۢا بَيْنَهُمْ ۗ وَمَن يَكْفُرْ بِـَٔايَـٰتِ ٱللَّهِ فَإِنَّ ٱللَّهَ سَرِيعُ ٱلْحِسَابِ',
-        'verseText': 'Certainly, Allah’s only Way is Islam.1 Those who were given the Scripture did not dispute ˹among themselves˺ out of mutual envy until knowledge came to them. Whoever denies Allah’s signs, then surely Allah is swift in reckoning.',
+        'verseText': 'Certainly, Allah\’s only Way is Islam.1 Those who were given the Scripture did not dispute ˹among themselves˺ out of mutual envy until knowledge came to them. Whoever denies Allah\’s signs, then surely Allah is swift in reckoning.',
         'surahAndAyah': '3:19'
     },
     'an-nisa':{
         'arabicVerseText': 'يَـٰٓأَيُّهَا ٱلَّذِينَ ءَامَنُوا۟ لَا تَأْكُلُوٓا۟ أَمْوَٰلَكُم بَيْنَكُم بِٱلْبَـٰطِلِ إِلَّآ أَن تَكُونَ تِجَـٰرَةً عَن تَرَاضٍۢ مِّنكُمْ ۚ وَلَا تَقْتُلُوٓا۟ أَنفُسَكُمْ ۚ إِنَّ ٱللَّهَ كَانَ بِكُمْ رَحِيمًۭا',
-        'verseText': 'O believers! Do not devour one another’s wealth illegally, but rather trade by mutual consent. And do not kill ˹each other or˺ yourselves. Surely Allah is ever Merciful to you.',
+        'verseText': 'O believers! Do not devour one another\’s wealth illegally, but rather trade by mutual consent. And do not kill ˹each other or˺ yourselves. Surely Allah is ever Merciful to you.',
         'surahAndAyah': '4:29'
     },
     'al-maidah':{
@@ -48,7 +33,7 @@ const surah = {
     },
     'al-araf':{
         'arabicVerseText': 'وَلَا تُفْسِدُوا۟ فِى ٱلْأَرْضِ بَعْدَ إِصْلَـٰحِهَا وَٱدْعُوهُ خَوْفًۭا وَطَمَعًا ۚ إِنَّ رَحْمَتَ ٱللَّهِ قَرِيبٌۭ مِّنَ ٱلْمُحْسِنِينَ',
-        'verseText': 'Do not spread corruption in the land after it has been set in order. And call upon Him with hope and fear. Indeed, Allah’s mercy is always close to the good-doers.',
+        'verseText': 'Do not spread corruption in the land after it has been set in order. And call upon Him with hope and fear. Indeed, Allah\’s mercy is always close to the good-doers.',
         'surahAndAyah': '7:56'
     },
     'al-anfal':{
@@ -58,9 +43,65 @@ const surah = {
     },
     'at-tawbah':{
         'arabicVerseText': 'يُرِيدُونَ أَن يُطْفِـُٔوا۟ نُورَ ٱللَّهِ بِأَفْوَٰهِهِمْ وَيَأْبَى ٱللَّهُ إِلَّآ أَن يُتِمَّ نُورَهُۥ وَلَوْ كَرِهَ ٱلْكَـٰفِرُونَ',
-        'verseText': 'They wish to extinguish Allah’s light1 with their mouths, but Allah will only allow His light to be perfected, even to the dismay of the disbelievers.',
+        'verseText': 'They wish to extinguish Allah\’s light1 with their mouths, but Allah will only allow His light to be perfected, even to the dismay of the disbelievers.',
         'surahAndAyah': '9:32'
-    },             
+    },            
+    'yunus':{
+        'arabicVerseText': 'وَمَا ظَنُّ ٱلَّذِينَ يَفْتَرُونَ عَلَى ٱللَّهِ ٱلْكَذِبَ يَوْمَ ٱلْقِيَـٰمَةِ ۗ إِنَّ ٱللَّهَ لَذُو فَضْلٍ عَلَى ٱلنَّاسِ وَلَـٰكِنَّ أَكْثَرَهُمْ لَا يَشْكُرُونَ',
+        'verseText': 'What do those who fabricate lies against Allah expect on Judgment Day? Surely Allah is ever Bountiful to humanity, but most of them are ungrateful.',
+        'surahAndAyah': '10:60'
+    }, 
+    'hud':{
+        'arabicVerseText': 'الٓر ۚ كِتَـٰبٌ أُحْكِمَتْ ءَايَـٰتُهُۥ ثُمَّ فُصِّلَتْ مِن لَّدُنْ حَكِيمٍ خَبِيرٍ',
+        'verseText': 'Alif-Lãm-Ra. ˹This is˺ a Book whose verses are well perfected and then fully explained. ˹It is˺ from the One ˹Who is˺ All-Wise, All-Aware.',
+        'surahAndAyah': '11:1'
+    },  
+    'yusuf':{
+        'arabicVerseText': '',
+        'verseText': '',
+        'surahAndAyah': ''
+    },  
+    'ar-rad':{
+        'arabicVerseText': '',
+        'verseText': '',
+        'surahAndAyah': ''
+    },  
+    'ibrahim':{
+        'arabicVerseText': 'وَمَثَلُ كَلِمَةٍ خَبِيثَةٍۢ كَشَجَرَةٍ خَبِيثَةٍ ٱجْتُثَّتْ مِن فَوْقِ ٱلْأَرْضِ مَا لَهَا مِن قَرَارٍۢ ',
+        'verseText': 'And the parable of an evil word is that of an evil tree, uprooted from the earth, having no stability..',
+        'surahAndAyah': '14:26'
+    },
+    'al-hijr':{
+        'arabicVerseText': 'وَٱلْأَرْضَ مَدَدْنَـٰهَا وَأَلْقَيْنَا فِيهَا رَوَٰسِىَ وَأَنۢبَتْنَا فِيهَا مِن كُلِّ شَىْءٍۢ مَّوْزُونٍۢ',
+        'verseText': 'As for the earth, We spread it out and placed upon it firm mountains, and caused everything to grow there in perfect balance.',
+        'surahAndAyah': '15:19'
+    }, 
+    'an-nahl':{
+        'arabicVerseText': 'وَمَا بِكُم مِّن نِّعْمَةٍۢ فَمِنَ ٱللَّهِ ۖ ثُمَّ إِذَا مَسَّكُمُ ٱلضُّرُّ فَإِلَيْهِ تَجْـَٔرُونَ',
+        'verseText': 'Whatever blessings you have are from Allah. Then whenever hardship touches you, to Him ˹alone˺ you cry ˹for help˺.',
+        'surahAndAyah': '16:53'
+    }, 
+    'al-isra':{
+        'arabicVerseText': 'وَيَخِرُّونَ لِلْأَذْقَانِ يَبْكُونَ وَيَزِيدُهُمْ خُشُوعًۭا ۩',
+        'verseText': 'And they fall down upon their faces weeping, and it increases them in humility.',
+        'surahAndAyah': '17:109'
+    }, 
+    'al-kahf':{
+        'arabicVerseText': 'إِنَّا جَعَلْنَا مَا عَلَى ٱلْأَرْضِ زِينَةًۭ لَّهَا لِنَبْلُوَهُمْ أَيُّهُمْ أَحْسَنُ عَمَلًۭا',
+        'verseText': 'We have indeed made whatever is on earth as an adornment for it, in order to test which of them is best in deeds.',
+        'surahAndAyah': '18:7'
+    }, 
+    'maryam':{
+        'arabicVerseText': 'فَنَادَىٰهَا مِن تَحْتِهَآ أَلَّا تَحْزَنِى قَدْ جَعَلَ رَبُّكِ تَحْتَكِ سَرِيًّۭا',
+        'verseText': 'So a voice reassured her from below her, “Do not grieve! Your Lord has provided a stream at your feet.',
+        'surahAndAyah': '19:24'
+    },
+    'taha':{
+        'arabicVerseText': 'إِنَّهُۥ مَن يَأْتِ رَبَّهُۥ مُجْرِمًۭا فَإِنَّ لَهُۥ جَهَنَّمَ لَا يَمُوتُ فِيهَا وَلَا يَحْيَىٰ',
+        'verseText': 'But whoever comes to Him as a believer, having done good, they will have the highest ranks:',
+        'surahAndAyah': '20:75'
+    }, 
+     
     'unknown':{
         'verseText': 'unknown',
         'surahAndAyah': 'unknown'
